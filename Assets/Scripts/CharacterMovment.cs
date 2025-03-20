0776a57d3a6de3e4ceedbcf95c197808c0841a87
Zander-Driesenga-student <zadr09b@student.oaisd.org>
2025-03-20 09:54:52 -0400

    Player Movement

---
+++ Assets/Scripts/CharacterMovment.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovment : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float moveSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = FindAnyObjectByType<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        float rightInput = Input.GetAxis("Horizontal");
+
+        Vector3 forward = Camera.main.transform.forward;
+        Vector3 right = Camera.main.transform.right;
+
+        forward.y = 0f;
+        right.y = 0f;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 moveDirection = (forwardInput * forward) + (rightInput * right);
+        moveDirection.Normalize();
+        moveDirection.y = -1f;
+
+        characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
+    }
+}
